d42a24013798511f47606e86d71951191ebd8963
Decode Marines <goipukraine@gmail.com>
2021-10-30 17:41:11 +0300

    document added with language option

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { DocumentInitialProps, DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
+    }
+    render(): JSX.Element {
+        return (
+            <Html lang="en">
+                <Head>
+                    <body>
+                        <Main />
+                        <NextScript />
+                    </body>
+                </Head>
+            </Html>
+        );
+    }
+}
+export default MyDocument;
